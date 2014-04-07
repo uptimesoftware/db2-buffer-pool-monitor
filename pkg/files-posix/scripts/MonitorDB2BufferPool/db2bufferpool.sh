7f45759accbf8adffdd25ef8e930cf08f2758da4
Patrick Lui <patrick.lui@uptimesoftware.com>
2014-04-07 09:35:04 -0400

    Packaged for 7.3

--- pkg/files-posix/scripts/MonitorDB2BufferPool/db2bufferpool.sh
+++ pkg/files-posix/scripts/MonitorDB2BufferPool/db2bufferpool.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-../../jre/bin/java -classpath .;lib -jar db2bufferpool.jar "$UPTIME_HOSTNAME" "$UPTIME_DBPORT" "$UPTIME_DBUSER" "$UPTIME_DBPASSWORD" "$UPTIME_DBNAME"
+../../jre/bin/java -classpath .:lib -jar db2bufferpool.jar "$UPTIME_HOSTNAME" "$UPTIME_DBPORT" "$UPTIME_DBUSER" "$UPTIME_DBPASSWORD" "$UPTIME_DBNAME"
